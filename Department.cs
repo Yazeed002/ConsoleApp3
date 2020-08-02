a51edf6047a5216e420aba8d349e623574fb7667
Yazeed <yaz.aloqaily@gmail.com>
2020-08-02 04:54:50 +0300

    Add project files.

---
+++ Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+
+        public Department(string name)
+        {
+
+            Name = name;
+
+        }
+    }
+
+
+        
+
+         
+}
